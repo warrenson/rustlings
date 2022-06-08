9a84f2cc7a3038865e057ac27d4e556b82c9743c
Ben warren <polarisinc@gmail.com>
2022-06-08 22:11:43 +1200

    completed so far

--- exercises/error_handling/errors4.rs
+++ exercises/error_handling/errors4.rs
@@ -14,6 +14,13 @@ enum CreationError {
 
 impl PositiveNonzeroInteger {
     fn new(value: i64) -> Result<PositiveNonzeroInteger, CreationError> {
+        if value == 0 {
+            return Err(CreationError::Zero)
+        };
+        if value < 0 {
+            return Err(CreationError::Negative)
+        };
+
         Ok(PositiveNonzeroInteger(value as u64))
     }
 }
--- exercises/functions/functions1.rs
+++ exercises/functions/functions1.rs
@@ -1,8 +1,13 @@
 // functions1.rs
 // Make me compile! Execute `rustlings hint functions1` for hints :)
 
-// I AM NOT DONE
-
 fn main() {
-    call_me();
+    let x = call_me();
+    println!("Got: {}", x);
+}
+
+fn call_me() -> String {
+    const r: &str = "called";
+    println!("{}", r);
+    return r.to_string();
 }
